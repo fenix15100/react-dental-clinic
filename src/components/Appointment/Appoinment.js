cfa790537ec6fba8372132cf18e35b1ff3504264
fenix15100 <francisco.camacho15@gmail.com>
2020-01-02 17:57:59 +0100

    Render Apoiments

--- src/components/Appointment/Appoinment.js
+++ src/components/Appointment/Appoinment.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Appoinment = () => {
+const Appoinment = (props) => {
     return (
+        <div className="media mt-3">
+            <div className="media-body">
+                <h3 className="mt-0">Nombre: {props.appoinment.nombre}</h3>
+                <p className="card-text">Dni: {props.appoinment.dni}</p>
+                <p className="card-text">Fecha: {props.appoinment.fecha}</p>
+                <p className="card-text">Hora: {props.appoinment.hora}</p>
+                <p className="card-text">Observaciones: {props.appoinment.observaciones}</p>
+            </div>
+        </div>
         
     );
 }
--- src/components/Appointment/ListAppoinment.js
+++ src/components/Appointment/ListAppoinment.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import Appoinment from './Appoinment'
 
 const ListAppoinment = (props) => {
     return (
         <div className="card mt-2 py-5">
             <div className="card-body">
                 <h2 className="card-title tex-center">
-                    Administra las citas aqui.
+                    Administra las citas aqui:
                 </h2>
                 <div className="lista-citas">
-                    {props.appoimments.map((value)=>{
-
-
-
-                    })}
-
+                {props.appoimments.map((value)=>(
+                            <Appoinment
+                                key={value.id}
+                                appoinment={value}
+                            />
+                    ))} 
                 </div>
             </div>
 
