bba9d67cf06c50e2d7778fecc004f71b3c40c575
fenix15100 <francisco.camacho15@gmail.com>
2020-01-02 17:59:29 +0100

    fix typo

--- src/components/Appointment/Appoinment.js
+++ src/components/Appointment/Appoinment.js
@@ -5,7 +5,7 @@ const Appoinment = (props) => {
         <div className="media mt-3">
             <div className="media-body">
                 <h3 className="mt-0">Nombre: {props.appoinment.nombre}</h3>
-                <p className="card-text">Dni: {props.appoinment.dni}</p>
+                <p className="card-text">DNI: {props.appoinment.dni}</p>
                 <p className="card-text">Fecha: {props.appoinment.fecha}</p>
                 <p className="card-text">Hora: {props.appoinment.hora}</p>
                 <p className="card-text">Observaciones: {props.appoinment.observaciones}</p>
