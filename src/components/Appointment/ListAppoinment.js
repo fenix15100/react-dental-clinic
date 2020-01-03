c18b88ed4647c6f8b5508b02a8c3cc7cf0c7d3af
fenix15100 <francisco.camacho15@gmail.com>
2020-01-03 13:41:10 +0100

    Fix conditional <h2> in ListAppoinment.js

--- src/components/Appointment/ListAppoinment.js
+++ src/components/Appointment/ListAppoinment.js
@@ -6,9 +6,11 @@ const ListAppoinment = ({appoimments,destroyAppoiment}) => {
     return (
         <div className="card mt-2 py-5">
             <div className="card-body">
-                <h2 className="card-title tex-center">
-                    Administra las citas aqui:
-                </h2>
+                {listappoi.length>0
+                    ?<h2 className="card-title tex-center">Administra las citas aqui:</h2>
+                    :<h2 className="card-title tex-center">No hay citas:</h2>
+                }
+                
                 <div className="lista-citas">
                 {listappoi.map((value)=>(
                             <Appoinment
